c0cf254a0e00a9fa9c7ca18fa0109f75fedd8361
sabinograzielly <grzllsabino@gmail.com>
2023-08-22 10:56:40 -0300

    melhorando a calculadora

--- calculadora.c
+++ calculadora.c
@@ -4,18 +4,37 @@
 // long int usa lld%d
 // unsigned long long int é um númer muito grande
 // multiplicação * 
+// numero1++ incrementa primeiro 
+//numero1++ incrementa depois 
   int main(){
     int numero1;
     int numero2;
     int resultado;
+    char operacao; 
 
-    numero1 = 20;
-    numero2 = 3;
+    printf("Insira uma operacao: \n");
+    scanf("%c", &operacao);
 
-    resultado = numero1 + numero2;
-    printf("Resultado: %d\n", resultado);
+    printf("Insira dois numeros:\n");
+    scanf("%d", &numero1);
+    scanf("%d", &numero2);
 
-    
+    printf("Operacao: %c\n", operacao);
+
+    if(operacao == '+')// caracter é com aspas simples
+    {
+    printf("Resultado: %d\n", numero1 + numero2);
+    }else if (operacao == '-'){
+    printf("Resultado: %d\n",numero1 - numero2);
+    }else if (operacao == '*'){
+       printf("Resultado: %d\n",resultado = numero1 * numero2);
+    }else if (operacao == '/'){
+        printf("Resultado: %d\n",resultado = numero1 / numero2);
+    }else if (operacao == '%'){
+        printf("Resultado: %d\n",resultado = numero1 % numero2);
+    } else{
+      printf("OPERAÇÃO INVALIDA!!!!\n");
+    }
 
 return 0;
 
