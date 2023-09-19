a075e6e5a2f67787d958857ae217f593e41c6b09
sabinograzielly <grzllsabino@gmail.com>
2023-09-19 10:48:17 -0300

    organ

--- listaAvaliativa01/questao02.c
+++ listaAvaliativa01/questao02.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
 int main(){
   int N;
+  N= 0 >= N >= 1000000000
 
-printf("insira um numero: \n");
+printf("insira N: \n");
 scanf ("%d", &N);
+
+if %2 !=0;
+{
+  printf("%d eh par \n", N);}
+else {
+      printf(" %d eh impar \n",N);
+    }
 
 return 0;
 }
