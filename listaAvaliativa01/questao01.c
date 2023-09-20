ffc750ecfd6f12f0b2b37521af1f11c8df9e9c74
sabinograzielly <grzllsabino@gmail.com>
2023-09-20 13:32:11 -0300

    tentando

--- listaAvaliativa01/questao01.c
+++ listaAvaliativa01/questao01.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 int main(){
 
-int X;
+int X, naoTeve; 
+
 scanf ("%d", &X);
 
-while (X == 1942 || 1946 || 1916 || 1940 || 1944){
+for (naoTeve = 1940;naoTeve<=1946 ; naoTeve = naoTeve + 2 || X % 4 == 1 && X<=1895 || X == 1916){
     printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d \n ", X);
-   
-   break;
-   }
-if(X % 4 == 0 && X >= 1896){
+   if(X % 4 == 0 && X >= 1896){
 printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d \n",X);
 }
-else if(X % 4 == 0 && X >= 1930){
+else if(X % 4 == 2 && X >= 1930){
     printf("A Copa do Mundo de Futebol ocorreu no ano de %d \n",X);
-}   
+} 
+   break;
+   }  
 
 return 0;
 }
