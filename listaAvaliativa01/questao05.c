891b66de6f7609c9883a58813f2cba4aa5967162
sabinograzielly <grzllsabino@gmail.com>
2023-09-28 23:59:14 -0300

    arrumamdo a 5

--- listaAvaliativa01/questao05.c
+++ listaAvaliativa01/questao05.c
@@ -2,29 +2,37 @@
 
 int main(){
 
-int A,B, multiplo;
+int A,B;
 scanf ("%d",&A);
 scanf ("%d",&B);
 
-multiplo = A%B;
 
 if(A==B){
     printf("Os valores lidos sao iguais \n");
 }
 else if (A > B){
     printf("%d eh maior que %d \n" ,A,B);
-    if (multiplo==0){
-        printf("%d eh multiplo de %d \n",A,B );
+    if (B==0){
+        printf("%d nao eh multiplo de %d \n",A,B );
     }
+    else if(A % B == 0)
+        {
+            printf("%d eh multiplo de %d\n",A,B);
+        }
     else {
         printf("%d nao eh multiplo de %d \n", A,B);
     }
 }
 else{
     printf("%d eh maior que %d \n" ,B,A);
-    if (multiplo==0){
+    if (A==0){
         printf("%d eh multiplo de %d \n",B,A );
     }
+     else if (B%A == 0)
+        {
+           printf("%d eh multiplo de %d\n",B,A);
+           }
+
     else {
         printf("%d nao eh multiplo de %d \n", B,A);
     }
