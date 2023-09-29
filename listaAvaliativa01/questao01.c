6ae063e8d1014622cf7b7950beba4bd6480a56f4
sabinograzielly <grzllsabino@gmail.com>
2023-09-29 00:20:15 -0300

    arrumando a 1

--- listaAvaliativa01/questao01.c
+++ listaAvaliativa01/questao01.c
@@ -2,7 +2,7 @@
 int main(){
 int X;
 scanf ("%d", &X);
-if(X==1916||X==1940||X==1942||X==1944||X==1946){
+if(X <= 1895 || X==1916||X==1940||X==1942||X==1944||X==1946 || X % 4 == 1 ){
 printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d. \n ", X);
    }  
 else{
@@ -12,9 +12,6 @@ printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d. \n",X);
 if(X % 4 == 2 && X >= 1930 && X!=1916 && X!=1940 && X!=1942 && X!=1944 && X!=1946){
     printf("A Copa do Mundo de Futebol ocorreu no ano de %d. \n",X);
 }
-    else {
-        printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d. \n ", X);
-    }
 } 
 return 0;
 }
