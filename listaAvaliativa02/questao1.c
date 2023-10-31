9950f9023a79082dac1d6a82d3b0147f216f69fe
sabinograzielly <137621247+sabinograzielly@users.noreply.github.com>
2023-10-31 14:08:44 -0300

    Create questao1.c

---
+++ listaAvaliativa02/questao1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int valor,valores[10];
+    int tamanhoInicial = 10;
+    
+    
+    for (int i = 0; i < tamanhoInicial; i++){
+    scanf("%d", &valor);
+         
+
+    while (tamanhoInicial > 1) {
+        for (int i = 0; i < tamanhoInicial - 1; i++) {
+            printf("%d%c", valores[i] + valores[i + 1], i < tamanhoInicial - 2 ? ' ' : '\n');// perguntar se pode usar o ? bibi disse que é um divisor de aguas, antes dele verdadeiro e depois falso.
+            valores[i] += valores[i + 1];
+        }
+        tamanhoInicial--;
+    }
+
+    return 0;
+}}
