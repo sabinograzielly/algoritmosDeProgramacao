6d69901d8279720919ddf07c1a96aa64af53cea8
sabinograzielly <grzllsabino@gmail.com>
2023-11-14 10:44:45 -0300

    tentando1

--- listaAvaliativa03/Questao02.c
+++ listaAvaliativa03/Questao02.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 
+void valorFinal(int meses, double aporte, double taxaRetorno) {
+    aporte *(1 + taxaRetorno) * (((pow(1 + taxaRetorno,meses) - 1)/taxaRetorno));
+
+for (int i = 0; i <= meses; i++)
+{ 
+   printf("Montante ao fim do mes %d: R$ %.2f\n", i, valorFinal);
+   return;
+}
+
+}
 int main(){
 
 int meses; 
-double aporte;
+double aporte, taxaRetorno;
+
+scanf("%d", &meses);
+scanf("%lf", &aporte);
+scanf("%lf", &taxaRetorno);
 
 
+valorFinal(meses, aporte, taxaRetorno);
+      
 
-
-
-
-
-    return 0;
+return 0;
 }
