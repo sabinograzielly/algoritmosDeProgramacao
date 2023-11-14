45e132ecc05a097411a0c89527baa925acd0de02
sabinograzielly <grzllsabino@gmail.com>
2023-11-14 10:46:45 -0300

    .

--- listaAvaliativa03/Questao02.c
+++ listaAvaliativa03/Questao02.c
@@ -4,9 +4,7 @@ void valorFinal(int meses, double aporte, double taxaRetorno) {
     aporte *(1 + taxaRetorno) * (((pow(1 + taxaRetorno,meses) - 1)/taxaRetorno));
 
 for (int i = 0; i <= meses; i++)
-{ valorFinal(int meses, double aporte, double taxaRetorno) {
-    aporte *(1 + taxaRetorno) * (((pow(1 + taxaRetorno,meses) - 1)/taxaRetorno));
-
+{ 
    printf("Montante ao fim do mes %d: R$ %.2f\n", i, valorFinal);
    return;
 }
